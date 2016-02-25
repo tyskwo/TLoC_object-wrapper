0d7b4dd8ae0d9a7e314a4af5aeb986e6fa2ebea3
Ty Wood <tyskwo@gmail.com>
2016-02-25 10:11:07 -0500

    [CHANGED] version number, reverted project name.

--- tlocUserVars.cmake
+++ tlocUserVars.cmake
@@ -3,7 +3,7 @@ if(${SOLUTION_NAME} STREQUAL "solution")
   project(${SOLUTION_NAME})
 endif()
 
-set(SOLUTION_EXECUTABLE_PROJECTS "lighting;")
+set(SOLUTION_EXECUTABLE_PROJECTS "skopworks;")
 #list(APPEND SOLUTION_EXECUTABLE_PROJECTS "nextExeProject;")
 
 set(SOLUTION_LIBRARY_PROJECTS "skopworksLib;")
