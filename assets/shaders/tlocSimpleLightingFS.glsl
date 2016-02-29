c62a455956fe9bff6ea89ad507916efa575779c3
tyskwo <tyler.wood@mymail.champlain.edu>
2016-02-29 10:50:09 -0500

    [ADDED] lighting to shader.

--- assets/shaders/tlocSimpleLightingFS.glsl
+++ assets/shaders/tlocSimpleLightingFS.glsl
@@ -1,10 +1,11 @@
 #version 330 core
 
 // We need one out (used to be g_FragColor)
+in vec3 v_color;
 out vec3 o_color;
 
 void main()
 {
   // NOTE: Tex co-ords flipped in 't'
-	o_color = vec3(1.0, 0.0, 0.0);
+	o_color = v_color;
 }
