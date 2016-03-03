60a502feea9f8d0d797e6c645b26fd1f93772853
tyskwo <tyler.wood@mymail.champlain.edu>
2016-03-03 10:46:04 -0500

    [ADDED] per pixel lighting. the ability to move the light position outside of the shader.
    [IN PROGRESS]
    working on breaking up the main method.

--- assets/shaders/tlocSimpleLightingFS.glsl
+++ assets/shaders/tlocSimpleLightingFS.glsl
@@ -1,11 +1,13 @@
 #version 330 core
 
-// We need one out (used to be g_FragColor)
-in vec3 v_color;
-out vec3 o_color;
+//the color from the vertex shader
+	in  vec3 v_color;
+
+//the color to pass to the renderer
+	out vec3 o_color;
 
 void main()
 {
-  // NOTE: Tex co-ords flipped in 't'
+//simply just pass the color
 	o_color = v_color;
 }
--- src/skopworks/src/main.cpp
+++ src/skopworks/src/main.cpp
@@ -7,79 +7,134 @@
 #include <gameAssetsPath.h>
 
 
+
 //namespace
 using namespace tloc;
+
 
 
 //shader paths
 namespace 
 {
-    core_str::String shaderPathVS("/shaders/tlocSimpleLightingVS.glsl");
-    core_str::String shaderPathFS("/shaders/tlocSimpleLightingFS.glsl");
+    core_str::String shaderPathVS("/shaders/tlocSimpleLightingVS_perFragment.glsl");
+    core_str::String shaderPathFS("/shaders/tlocSimpleLightingFS_perFragment.glsl");
 
 	const core_str::String g_assetsPath(GetAssetsPath());
 };
 
+
+
+
+
 /////////////////////////////////////////////////////////////////////////
 // lighting sample
+
 class Program : public Application
 {
 public:
 	Program() : Application("lighting example") { }
 
+
+
 private:
+
+//vertices of the sphere
+	gfx_med::ObjLoader::vert_cont_type vertices;
+
+//position of the light
+	gfx_gl::uniform_vso u_lightPosition;
+
+
+
+
 	//after calling the constructor
 	error_type Post_Initialize() override
 	{
 
-	//get the path to the object file
-		auto path = core_io::Path(core_str::String(GetAssetsPath()) + "/models/sphere.obj");
-			//any place you want to pass a string or const char, use a BufferArg, converts to and from both.
+	//load the object.
+		loadSphere();
 
-	//open up the .obj file, and report error if necessary
-		core_io::FileIO_ReadA objFile(path);
-		if (objFile.Open() != ErrorSuccess)
-		{
-			TLOC_LOG_GFX_ERR() << "Could not open " << path;
-			return ErrorFailure;
-		}
-
-	//get contents of the .obj file
-		core_str::String objFileContents;
-		objFile.GetContents(objFileContents);
-
-	//try loading the object, and check for parsing errors
-		gfx_med::ObjLoader objLoader;
-		if (objLoader.Init(objFileContents) != ErrorSuccess)
-		{
-			TLOC_LOG_GFX_ERR() << "Failed to parse .obj file.";
-			return ErrorFailure;
-		}
-
-
-
-
-		//auto&
 	//get the scene from the application
 		auto			  scene = GetScene();
 						  scene->AddSystem<gfx_cs::MaterialSystem>();	//add material system
 						  scene->AddSystem<gfx_cs::CameraSystem>();		//add camera
 		auto meshSystem = scene->AddSystem<gfx_cs::MeshRenderSystem>();	//add mesh render system
 
+		scene->
+
 	//set renderer
 		meshSystem->SetRenderer(GetRenderer());
 
-
-	//unpack the vertices of the object
-		gfx_med::ObjLoader::vert_cont_type vertices;
-		{ objLoader.GetUnpacked(vertices, 0); }
+		setLightPosition();
 
 	//create mesh with the vertices, and create material with the shaders.
 		auto meshEntity = scene->CreatePrefab<pref_gfx::Mesh>().Create(vertices);
 						  scene->CreatePrefab<pref_gfx::Material>().AssetsPath(GetAssetsPath())
+																   .AddUniform(u_lightPosition.get())
 																   .Add(meshEntity, core_io::Path(shaderPathVS), core_io::Path(shaderPathFS));
 
-	//add camera
+	
+
+	//set the camera
+		meshSystem->SetCamera(cameraEntity);
+
+
+
+		return Application::Post_Initialize();
+	}
+
+
+
+
+	core_io::Path getPathOfSphere()
+	{
+	//get the path to the object file
+		return core_io::Path(core_str::String(GetAssetsPath()) + "/models/sphere-smooth.obj");
+			//any place you want to pass a string or const char, use a BufferArg, converts to and from both.
+	}
+
+
+
+
+	void loadSphere()
+	{
+	//open up the .obj file, and report error if necessary
+		core_io::FileIO_ReadA objFile(getPathOfSphere());
+		if (objFile.Open() != ErrorSuccess)
+		{
+			TLOC_LOG_GFX_ERR() << "Could not open " << getPathOfSphere();
+		}
+
+
+	//get contents of the .obj file
+		core_str::String objFileContents;
+		objFile.GetContents(objFileContents);
+
+
+	//try loading the object, and check for parsing errors
+		gfx_med::ObjLoader objLoader;
+		if (objLoader.Init(objFileContents) != ErrorSuccess)
+		{
+			TLOC_LOG_GFX_ERR() << "Failed to parse .obj file.";
+		}
+
+
+	//unpack the vertices of the object
+		{ objLoader.GetUnpacked(vertices, 0); }
+	}
+
+	void setLightPosition()
+	{
+	//set the light's position
+		u_lightPosition->SetName("u_lightPosition").SetValueAs(math_t::Vec3f32(1.0f, 1.0f, 3.0f));
+	}
+
+	void createCamera()
+	{
+		auto scene = GetScene();
+
+
+		//add camera
 		auto cameraEntity = scene->CreatePrefab<pref_gfx::Camera>()
 			.Perspective(true)
 			.Near(0.1f)
@@ -87,14 +142,12 @@ private:
 			.VerticalFOV(math_t::Degree(60.0f))
 			.Create(GetWindow()->GetDimensions());
 
-	//change camera's position
+		//change camera's position
 		cameraEntity->GetComponent<math_cs::Transform>()->SetPosition(math_t::Vec3f32(0, 0, 5));
-
-		meshSystem->SetCamera(cameraEntity);
-
-		return Application::Post_Initialize();
 	}
 };
+
+
 
 
 
