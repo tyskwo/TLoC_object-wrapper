4b11353db23f542548205ffe4d4a6658f61c1342
Ty Wood <tyskwo@gmail.com>
2016-03-03 10:46:04 -0500

    [ADDED] per pixel lighting. the ability to move the light position outside of the shader.
    [IN PROGRESS]
    working on breaking up the main method.

--- assets/shaders/tlocSimpleLightingVS.glsl
+++ assets/shaders/tlocSimpleLightingVS.glsl
@@ -1,28 +1,37 @@
 #version 330 core
 
-// Input vertex data, different for all executions of this shader.
-in vec3 a_vertPos;
-in vec3 a_vertNorm;
+//Input vertex data, different for all executions of this shader.
+//the position and normal of each vertice
+	in      vec3 a_vertPos;
+	in      vec3 a_vertNorm;
 
-uniform mat4 u_vp;
-uniform mat4 u_model;
+//the constants over the entire model: the model and vertice matrix
+	uniform mat4 u_vp;
+	uniform mat4 u_model;
 
-vec3 lightPosition;
-vec3 color;
+//the light position and the color of the sphere
+	        vec3 lightPosition;
+	        vec3 color;
 
-out vec3 v_color;
+//color to pass to the fragment shader
+	out     vec3 v_color;
 
 void main()
 { 
-	lightPosition = vec3(1.0, 1.0, 1.0);
-	color = vec3(1.0, 0.0, 0.0);
+//set the light position and the color of the sphere
+	lightPosition = vec3(1.0, 1.0, 2.0);
+	color		  = vec3(1.0, 0.0, 0.0);
 
+//determine the position of the vertice
 	gl_Position = u_vp * u_model * vec4(a_vertPos, 1);
 
+//determine the normal of the light's direction
 	vec3 lightDirection = lightPosition - a_vertPos;
 	lightDirection = normalize(lightDirection);
 
+//get the diffuse multiplier
 	float diffuseMultiplier = dot(a_vertNorm, lightDirection);
 
+//pass the calculated color to the fragment shader
 	v_color = diffuseMultiplier * color;
 }
